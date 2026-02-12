875c1c07660d4c5fb4439389bb67a9df30c789b1
Edmilson DA COSTA SA <edmilson.sadacosta@gmail.com>
2026-02-12 13:46:41 +0100

    Première implémentation des strategies

--- Vaches/domain/PieNoir.py
+++ Vaches/domain/PieNoir.py
@@ -17,16 +17,11 @@ class PieNoire (VacheALait):
     _ration : dict[TypeNourriture, float]
 
     def __init__(self, petit_nom, poids, age, nb_taches_noires, nb_taches_blanches):
-        super().__init__(petit_nom, poids, age)
         self.nb_taches_blanches = nb_taches_blanches
         self.nb_taches_noires = nb_taches_noires
+        super().__init__(petit_nom, poids, age)
         self._ration = {}
-        if type(self.nb_taches_noires) != int or type(self.nb_taches_blanches) != int:
-            raise InvalidVacheException ("Le nombre de taches noires ou blanches doit etre un entier") 
-        elif self.nb_taches_blanches <= 0:
-            raise InvalidVacheException("Nombre de taches blanches nul ou negatif")
-        elif self.nb_taches_noires <= 0:
-            raise InvalidVacheException("Nombre de taches noires nul ou negatif") 
+        self.valider_etat()
         
     @property
     def ration(self) -> dict[TypeNourriture, float]:
@@ -57,3 +52,15 @@ class PieNoire (VacheALait):
     def __str__(self):
         message_de_base = super().__str__()
         return f"{message_de_base} (C'est une Pie Noire avec {self.nb_taches_noires} taches noires et {self.nb_taches_blanches} taches blanches)"
+    
+    def valider_taches(self):
+        if type(self.nb_taches_noires) != int or type(self.nb_taches_blanches) != int:
+            raise InvalidVacheException ("Le nombre de taches noires ou blanches doit etre un entier") 
+        elif self.nb_taches_blanches <= 0:
+            raise InvalidVacheException("Nombre de taches blanches nul ou negatif")
+        elif self.nb_taches_noires <= 0:
+            raise InvalidVacheException("Nombre de taches noires nul ou negatif")
+        
+    def valider_etat(self):
+        super().valider_etat()
+        self.valider_taches()
